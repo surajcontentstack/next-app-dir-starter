d6dcec60964cf41d08a2dbb7e28e0951a73c3ceb
Contentstack-AnkitaD <ankita.dodamani@contentstack.com>
2023-11-29 13:37:31 +0530

    fix: Added Header Footer and Hero Banner component

---
+++ src/app/components/about-us/page.tsx
@@ -0,0 +1,7 @@
+export const Aboutus = () => {
+    return (
+        <header className="Aboutus">
+            <h1 className="text-3xl font-bold underline">Aboutus</h1>
+        </header>
+    );
+};
